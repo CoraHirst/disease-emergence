8302579cb20a872f60c8bd838931bc98e6c83183
corahirst <cora.virginia.hirst@gmail.com>
2025-01-14 16:38:18 -0500

    edited functions to include fixed step model
    functions.r now includes a fixed step model, where increases in R0 each mutation is a fixed step size. Included demonstrations of the function in fv_multiple_mutations.qmd

--- code/functions/functions.R
+++ code/functions/functions.R
@@ -58,7 +58,7 @@ pEmergence_jackpot = function(m, mu, R0_1, R0_2, xstart, init){ #m is number of 
     for(i in 1:(m-1)){
       y[i] = exp(-(1-mu)*(R0_1)*(1-x[i]))*exp(-mu*R0_1*(1-x[i+1])) - x[i]
     }
-    y[ms[j]] = exp(-R0_2*(1-x[m])) - x[m]
+    y[m] = exp(-R0_2*(1-x[m])) - x[m]
     y
   }
   
@@ -141,43 +141,52 @@ pEmergence_additive = function(m, mu, R0_1, R0_2, xstart, init){ #m is number of
 
 
 #################### additive model ######################
-### multiple-mutation probability of extinction function with additive R0
-
-pEmergence_additive_fixedstep = function(m, mu, R0_1, R0_2, Rstep, xstart, init){ #m is number of types, mu is mutation rate, R0_1 is wt R0, Ro_2 is adapted type R0, xstart is a vector of initial guesses for fixed point equations (length(m)), and init is initial number of cases of each type
+### Additive (fixed-step) R0 model where wtR0 determines how far from final type
+################### probability of emergence with fixed increase in R0 each mutation ########################
+pEmergence_fixed = function(m, mu, R0_1, R0_2) {
+  ### calculate step size and breaks
+  Rstep = R0_2/(m-1) #increase in R0 with each mutation
+  breaks = Rstep*(1:(m)) #intervals describing the range of R0 for types (variants) with increasing number of mutations
   
-  ### define system of nl equations
-  single_mut <- function(x) {
-    y <- numeric(m) #vector to store fixed points
-    ### define fixed point equations
+  ### define R0 vector 
+  ## determine into which interval the wtR0 falls
+  diffs = breaks - R0_1 #distance from wtR0 to each interval break
+  type = which(diffs == min(diffs[which(diffs >= 0)]))[1] # position of the least positive distance gives the interval - type - into which the wtR0 falls
+  
+  ## calculate change in R0 from wt to each other variant
+  delta_R0 = Rstep*c(-(type-1):0, 1:(m-type)) #vector of changes from wtR0 to the R0 of each other variant
+  R0 = R0_1 + delta_R0 # add the changes from wtR0 to wtR0 to give R0 of each variant
+  
+  ### define probability of emergence
+  prob_emergence = function(qs,init) {1 - prod(qs^init)} # 1 - product of the probabilities of extinction for each starting linneage
+  
+  ### define fixed point equations from PGFs
+  ## system of nl equations to solve
+  multi_mut <- function(x) {
+    y <- numeric(m)
     for(i in 1:(m-1)){
-      y[i] = exp(-(1-mu)*(R0_1+Rstep*(1-i))*(1-x[i]))*exp(-mu*R0_1*(1-x[i+1])) - x[i] #all variants from wt to just before adapted variant
+      y[i] = exp(-(1-mu)*(R0[i])*(1-x[i]))*exp(-mu*R0[i]*(1-x[i+1])) - x[i]
     }
-    y[ms[j]] = exp(-R0_2*(1-x[m])) - x[m] #adapted variant
+    y[m] = exp(-R0[m]*(1-x[m])) - x[m]
     y
   }
   
-  # function for prob.emergence
-  prob_emergence = function(qs,init) {1 - prod(qs^init)} #qs are solutions to fixed point equation, init is vector of initial number of infections o each type  
+  ### define initial conditions
+  init = rep(0, m) #start the initial number of cases of each type to 0 
+  init[type] = 1 #except for 1 case of the starting type
   
-  # parameters
-  m = m #number of variant types (number of mutations required for emergence +1)
-  mu = mu #mutation rate
-  R0_1 = R0_1 #initial R0
-  R0_2 = R0_2 #final R0_2
+  ### define initial guess for fixed point vector
+  xstart = rep(0,m) #easiest to set all probs to 0
+  xstart[type] = 1 #except for the prob of a linneage started by our 1 starting case type
   
-  # define initial conditions 
-  init = init #start with 1 wild type and no evolved
-  
-  # initial guess
-  xstart = xstart
-  
-  ### calculate fixed points and emergence prob
+  ### solve for fixed points
   # newton start
-  qs = nleqslv(xstart, single_mut, method="Newton", global="none", control=list(trace=1,stepmax=2))$x
-  # solve for prob.emergence
-  prob.emergence = prob_emergence(qs = qs, init = init) 
+  qs = nleqslv(xstart, multi_mut, method="Newton", global="none", control=list(trace=1,stepmax=2))$x
   
-  return(prob.emergence)
+  # solve for prob.emergence
+  prob.emergence = prob_emergence(qs = qs, init = init)
+  
+  return(prob.emergence) 
 }
 
 
