6bf45836e598704b39e30442df1d36a3715a42ba
corahirst <cora.virginia.hirst@gmail.com>
2025-02-22 17:17:36 -0500

    adaptiveevolution model
    added heatmaps for emergence times/probabilities

--- code/functions/archive/functions.R
+++ code/functions/archive/functions.R
@@ -189,3 +189,52 @@ pEmergence_fixed = function(m, mu, R0_1, R0_2, Rfinal) {
   
   return(prob.emergence) 
 }
+
+
+####################### adaptive evolution function in terms of m ##########################
+pEmergence_old = function(m, mu, R_wt, R_adapted) { #same function as above, but renamed parameters
+  ### calculate step size and breaks
+  Rstep = 1/(m-1) #increase in R0 with each mutation
+  breaks = Rstep*(1:(m)) #intervals describing the range of R0 for types (variants) with increasing number of mutations
+  
+  ### define R0 vector 
+  ## determine into which interval the wtR0 falls
+  diffs = breaks - R_wt #distance from wtR0 to each interval break
+  type = which(diffs == min(diffs[which(diffs >= 0)]))[1] # position of the least positive distance gives the interval - type - into which the wtR0 falls
+  
+  ## calculate change in R0 from wt to each other variant
+  delta_R0 = Rstep*c(-(type-1):0, 1:(m-type)) #vector of changes from wtR0 to the R0 of each other variant
+  R0 = R_wt + delta_R0 # add the changes from wtR0 to wtR0 to give R0 of each variant
+  R0[m] = R_adapted #final R0 >> 1 to account for differences in emergence probabilities arising from final R0
+  
+  ### define probability of emergence
+  prob_emergence = function(qs,init) {1 - prod(qs^init)} # 1 - product of the probabilities of extinction for each starting linneage
+  
+  ### define fixed point equations from PGFs
+  ## system of nl equations to solve
+  multi_mut <- function(x) {
+    y <- numeric(m)
+    for(i in 1:(m-1)){
+      y[i] = exp(-(1-mu)*(R0[i])*(1-x[i]))*exp(-mu*R0[i]*(1-x[i+1])) - x[i]
+    }
+    y[m] = exp(-R0[m]*(1-x[m])) - x[m]
+    y
+  }
+  
+  ### define initial conditions
+  init = rep(0, m) #start the initial number of cases of each type to 0 
+  init[type] = 1 #except for 1 case of the starting type
+  
+  ### define initial guess for fixed point vector
+  xstart = rep(0,m) #easiest to set all probs to 0
+  xstart[type] = 1 #except for the prob of a linneage started by our 1 starting case type
+  
+  ### solve for fixed points
+  # newton start
+  qs = nleqslv(xstart, multi_mut, method="Newton", global="none", control=list(trace=1,stepmax=2))$x
+  
+  # solve for prob.emergence
+  prob.emergence = prob_emergence(qs = qs, init = init)
+  
+  return(prob.emergence) 
+}
--- code/functions/functions.R
+++ code/functions/functions.R
@@ -1,52 +1,6 @@
-pEmergence_fixed = function(m, mu, R_wt, R_adapted) {
-  ### calculate step size and breaks
-  Rstep = 1/(m-1) #increase in R0 with each mutation
-  breaks = Rstep*(1:(m)) #intervals describing the range of R0 for types (variants) with increasing number of mutations
-  
-  ### define R0 vector 
-  ## determine into which interval the wtR0 falls
-  diffs = breaks - R_wt #distance from wtR0 to each interval break
-  type = which(diffs == min(diffs[which(diffs >= 0)]))[1] # position of the least positive distance gives the interval - type - into which the wtR0 falls
-  
-  ## calculate change in R0 from wt to each other variant
-  delta_R0 = Rstep*c(-(type-1):0, 1:(m-type)) #vector of changes from wtR0 to the R0 of each other variant
-  R0 = R_wt + delta_R0 # add the changes from wtR0 to wtR0 to give R0 of each variant
-  R0[m] = R_adapted #final R0 >> 1 to account for differences in emergence probabilities arising from final R0
-  
-  ### define probability of emergence
-  prob_emergence = function(qs,init) {1 - prod(qs^init)} # 1 - product of the probabilities of extinction for each starting linneage
-  
-  ### define fixed point equations from PGFs
-  ## system of nl equations to solve
-  multi_mut <- function(x) {
-    y <- numeric(m)
-    for(i in 1:(m-1)){
-      y[i] = exp(-(1-mu)*(R0[i])*(1-x[i]))*exp(-mu*R0[i]*(1-x[i+1])) - x[i]
-    }
-    y[m] = exp(-R0[m]*(1-x[m])) - x[m]
-    y
-  }
-  
-  ### define initial conditions
-  init = rep(0, m) #start the initial number of cases of each type to 0 
-  init[type] = 1 #except for 1 case of the starting type
-  
-  ### define initial guess for fixed point vector
-  xstart = rep(0,m) #easiest to set all probs to 0
-  xstart[type] = 1 #except for the prob of a linneage started by our 1 starting case type
-  
-  ### solve for fixed points
-  # newton start
-  qs = nleqslv(xstart, multi_mut, method="Newton", global="none", control=list(trace=1,stepmax=2))$x
-  
-  # solve for prob.emergence
-  prob.emergence = prob_emergence(qs = qs, init = init)
-  
-  return(prob.emergence) 
-}
-
-
-pEmergence_Rstep = function(delta_R, mu, R_wt, R_adapted) {
+######################### adaptive evolution function in terms of increase in R0 with mutation, delta R
+######## evenly dispersed distribution described by generating functions for secondary cases
+pEmergence_deltaR = function(delta_R, mu, R_wt, R_adapted) {
   ### calculate number of variants
   m = ceiling((((1+10^-12)-R_wt)/delta_R) + 1) #number of types in the branching process #note that we need a slight overshoot of R0 = 1 to get supercritical
   
@@ -78,10 +32,10 @@ pEmergence_Rstep = function(delta_R, mu, R_wt, R_adapted) {
   
   ### solve for fixed points
   # newton start
-  qs = nleqslv(xstart, multi_mut, method="Newton", global="none", control=list(trace=1,stepmax=2))$x
+  qs = nleqslv(xstart, multi_mut, method="Newton", global="none", control=list(trace=1,stepmax=2))$x #solve system of nonlinear equations for fixed point
   
   # solve for prob.emergence
-  prob.emergence = prob_emergence(qs = qs, init = init)
+  prob.emergence = prob_emergence(qs = qs, init = init) #solve for 1-sum(q_i^init(i)) - should be q_1^1 for a single starting case of type 1 
   
   return(prob.emergence) 
 }
