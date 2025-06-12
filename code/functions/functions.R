526177041cc7b52b0a56d23284a76dd29d7c7e3d
Cora <cora.virginia.hirst@gmail.com>
2025-06-12 08:57:39 -0400

    adds a function for emergence probability
    analytic solution for emergence probability of highly transmissible variant from supercritical spillover

--- code/functions/functions.R
+++ code/functions/functions.R
@@ -72,4 +72,43 @@ non_extinction = function(Rwt) {
   return(P) #solve for nonextinction probability
 }
 
+######## evenly dispersed distribution described by generating functions for secondary cases
+pEmergence_supercrit_deltaR = function(delta_R, mu, R_wt, R_adapted) {
+  ### calculate number of variants
+  m = ceiling(((3 + 10^-12) - R_wt)/delta_R) + 1  #number of types in the branching process #note that we need a slight overshoot of R0 = 1 to get supercritical
   
+  ### define R0 vector 
+  R0 = R_wt + (delta_R)*(0:(m-1))  # add the changes from wtR0 to give R0 of each variant
+  R0[m] = R_adapted #final R0 >> 1 to account for differences in emergence probabilities arising from final R0
+  
+  ### define probability of emergence
+  prob_emergence = function(qs,init) {1 - prod(qs^init)} # 1 - product of the probabilities of extinction for each starting lineage
+  
+  ### define fixed point equations from PGFs
+  ## system of nl equations to solve
+  multi_mut <- function(x) { # for poisson dist. the mean is R_0 and the generating function is exp(R0(s -1))
+    y <- numeric(m)
+    for(i in 1:(m-1)){
+      y[i] = exp(-(1-mu)*(R0[i])*(1-x[i]))*exp(-mu*R0[i]*(1-x[i+1])) - x[i] # fixed point from probability generating function 
+    }
+    y[m] = exp(-R0[m]*(1-x[m])) - x[m] # can only make type m, regardless of mutation. 
+    y
+  }
+  
+  ### define initial conditions
+  init = rep(0, m) #start the initial number of cases of each type to 0 
+  init[1] = 1 #except for 1 case of the starting type
+  
+  ### define initial guess for fixed point vector
+  xstart = rep(0,m) #easiest to set all probs to 0
+  xstart[1] = 1 #except for the prob of a linneage started by our 1 starting case type 1
+  
+  ### solve for fixed points
+  # newton start
+  qs = nleqslv(xstart, multi_mut, method="Newton", global="none", control=list(trace=1,stepmax=2))$x #solve system of nonlinear equations for fixed point
+  
+  # solve for prob.emergence
+  prob.emergence = prob_emergence(qs = qs, init = init) #solve for 1-sum(q_i^init(i)) - should be q_1^1 for a single starting case of type 1 
+  
+  return(prob.emergence) 
+}
